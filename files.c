33c1abb3a9ad66c86454d29a41c065824797c50e
metus01 <carnelmetus@gmail.com>
2023-08-17 17:58:32 +0100

    Inspiration , explore the w option on the files

--- files.c
+++ files.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 int main()
 {
-    
+    FILE* file = NULL;
+    file = fopen("test.txt", "r+");
+    if(file != NULL)
+    {
+        printf("Fichier ouvert");
+        fputc('A' , file);
+        fclose(file);
+       
+    }else
+    {
+        printf("access denied to file test.txt");
+    }
     return 0;
 }
