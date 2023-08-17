efdf6342c4c55600177c7f4646cb7d25b6681d22
metus01 <carnelmetus@gmail.com>
2023-08-17 18:20:19 +0100

    fputs usage problem

--- files.c
+++ files.c
@@ -3,11 +3,12 @@
 int main()
 {
     FILE* file = NULL;
-    file = fopen("test.txt", "r+");
+    file = fopen("test.txt", "w");
     if(file != NULL)
     {
         printf("Fichier ouvert");
         fputc('A' , file);
+        fputs('Amen' , file);
         fclose(file);
        
     }else
